17fc5a6c3c8f6c31a0939db64198419efd9b2f6c
isadora stangarlin <isadora.stangarlin@avenuecode.com>
2020-08-14 23:52:55 -0300

    Users creation

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -5,12 +5,12 @@ import Appointment from '../models/Appointments';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({date, provider}: Request) : Promise<Appointment> {
+  public async execute({date, provider_id}: Request) : Promise<Appointment> {
     const appointmentRepository = getCustomRepository(AppointmentRepository);
     const appointmentDate = startOfHour(date);
 
@@ -20,7 +20,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate
     });
 
